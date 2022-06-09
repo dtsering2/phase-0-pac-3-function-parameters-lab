21093ea8a4999314de5f8348d5aa840e6dedf858
Tsering Tsering <dtsering2@gmail.com>
2022-06-08 20:53:28 -0400

    Done with Assignment

--- index.js
+++ index.js
@@ -2,24 +2,20 @@
 function introduction(name){
     return `Hi, my name is ${name}.`;
 }
-console.log(introduction("Aki"));
-console.log(introduction("Samip"));
+
 
 //Function with two parameters
 function introductionWithLanguage (name,language){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
-console.log(introductionWithLanguage("Aki","Ember.js"));
-console.log(introductionWithLanguage("Samip", "React"));
+
 
 //Function with two parameters and a default
 function introductionWithLanguageOptional(name, language = "JavaScript"){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
-console.log(introductionWithLanguageOptional("Gracie"));
 
 //Function with two parameters and a overriden default
 function introductionWithLanguageOptional(name, language = "JavaScript"){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
-console.log(introductionWithLanguageOptional("Gracie", "Python"));
